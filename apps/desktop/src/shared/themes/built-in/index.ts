c6fa267f06ea37a5098346075f81da1bcb33e521
Tamara Zuk <hi@tamarazuk.dev>
2026-03-21 14:02:25 -0400

    feat(themes): add Catppuccin built-in theme variants

--- apps/desktop/src/shared/themes/built-in/index.ts
+++ apps/desktop/src/shared/themes/built-in/index.ts
@@ -2,10 +2,23 @@ import type { Theme } from "../types";
 import { darkTheme } from "./ember";
 import { lightTheme } from "./light";
 import { monokaiTheme } from "./monokai";
+import { catppuccinMochaTheme } from "./catppuccin-mocha";
+import { catppuccinMacchiatoTheme } from "./catppuccin-macchiato";
+import { catppuccinFrappeTheme } from "./catppuccin-frappe";
+import { catppuccinLatteTheme } from "./catppuccin-latte";
+
 /**
  * All built-in themes
  */
-export const builtInThemes: Theme[] = [darkTheme, lightTheme, monokaiTheme];
+export const builtInThemes: Theme[] = [
+	darkTheme,
+	lightTheme,
+	monokaiTheme,
+	catppuccinMochaTheme,
+	catppuccinMacchiatoTheme,
+	catppuccinFrappeTheme,
+	catppuccinLatteTheme,
+];
 
 /**
  * Default theme ID
@@ -20,4 +33,12 @@ export function getBuiltInTheme(id: string): Theme | undefined {
 }
 
 // Re-export individual themes
-export { darkTheme, lightTheme, monokaiTheme };
+export {
+	darkTheme,
+	lightTheme,
+	monokaiTheme,
+	catppuccinMochaTheme,
+	catppuccinMacchiatoTheme,
+	catppuccinFrappeTheme,
+	catppuccinLatteTheme,
+};
